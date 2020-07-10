437059415a7b57debbfcf4bc4ca517b05d0723e6
Greg Dick <gregdick.dev@gmail.com>
2020-07-10 16:41:14 -0500

    gradle and scope updates

--- app/src/main/java/com/example/greg/nhldata/ui/main/hilt/ApiModule.kt
+++ app/src/main/java/com/example/greg/nhldata/ui/main/hilt/ApiModule.kt
@@ -9,6 +9,7 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ActivityComponent
 import dagger.hilt.android.components.ActivityRetainedComponent
+import dagger.hilt.android.scopes.ActivityRetainedScoped
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
@@ -16,6 +17,7 @@ import retrofit2.converter.moshi.MoshiConverterFactory
 @InstallIn(ActivityRetainedComponent::class)
 object ApiModule {
 
+    @ActivityRetainedScoped
     @Provides
     fun provideNhlApiService() : NhlApiService {
         return Retrofit.Builder()
